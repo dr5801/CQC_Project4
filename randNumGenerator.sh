a143fc23cd98ef60a13a81a9fc82aaca4b605ec7
Drew Rife <dr5801@cs.ship.edu>
2017-03-28 09:10:40 -0400

    changed minor formatting of how random numbers are printed to text file

--- randNumGenerator.sh
+++ randNumGenerator.sh
@@ -11,12 +11,14 @@ function create_folder()
 	fi
 }
 
-# generates random numbers from 0-9999 5000 times to random/random_numbers.txt
-# then append r to the file of numbers for the error to occur
+# generates random numbers from 0-9999 1999 times to random/random_numbers.txt
+# then append r to the file of numbers for the error to occurs at the 2000th line
+#then append another 2000 lines of random numbers for a total of 4000 lines to the file
 function generateRandomNumbersToFile()
 {
-	printf "$(shuf -i 0-9999 -n 5000) \n" > ${directory}/${file}
+	printf "$(shuf -i 0-9999 -n 1999) \n" > ${directory}/${file}
 	printf "r\n" >> ${directory}/${file}
+	printf "$(shuf -i 0-9999 -n 2000) \n" >> ${directory}/${file}
 }
 
 create_folder
